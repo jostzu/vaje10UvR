1d23a7217b50d01eb0025a324b3c18fa16a85ce5
Jost Zuzek <jost.zuzek@gmail.com>
2020-12-14 21:50:05 +0100

    Best program ever

---
+++ hello.py
@@ -0,0 +1,9 @@
+def hello(s):
+    print("sup mane, " + s)
+def sestevanje(a, b):
+    rez = 0
+    rez = rez + a
+    rez = rez + a
+    return rez
+hello("idk what im doing tbh")
+print(sestevanje(1, 3))
